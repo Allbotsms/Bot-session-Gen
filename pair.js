e1d8dff81ad184007fd6745d724686c77d8b3dce
Allbotsms <abdulmuqeet056056@gmail.com>
2025-08-22 08:31:17 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -56,7 +56,7 @@ router.get('/', async (req, res) => {
                     const credsPath = path.join(tempDir, 'creds.json');
                     const sessionData = fs.readFileSync(credsPath, 'utf8');
                     const base64 = Buffer.from(sessionData).toString('base64');
-                    const sessionId = "ARSLAN-MD~" + base64;
+                    const sessionId = "HASSNAIN-MD~" + base64;
 
                     await sock.sendMessage(sock.user.id, { text: sessionId });
 
@@ -66,17 +66,17 @@ router.get('/', async (req, res) => {
                             `▸ *Never share* your session ID\n` +
                             `▸ Join our WhatsApp Channel\n` +
                             `▸ Report bugs on GitHub\n\n` +
-                            `_Powered by ARSLAN-MD\n\n` +
+                            `_Powered by HASSNAIN-MD\n\n` +
                             `🔗 *Useful Links:*\n` +
-                            `▸ GitHub: https://github.com/Arslan-MD/Arslan_MD\n` +
-                            `▸ https://whatsapp.com/channel/0029VarfjW04tRrmwfb8x306`,
+                            `▸ GitHub: https://github.com/kinghacker756/kinghacker756\n` +
+                            `▸ https://whatsapp.com/channel/0029Vb5Wj7A1iUxVukeiWx21`,
                         contextInfo: {
                             mentionedJid: [sock.user.id],
                             forwardingScore: 1000,
                             isForwarded: true,
                             forwardedNewsletterMessageInfo: {
                                 newsletterJid: "120363348739987203@newsletter",
-                                newsletterName: "ARSLAN-MD",
+                                newsletterName: "HASSNAIN-MD",
                                 serverMessageId: 143
                             }
                         }
